62ba1490d8967e5057f503b97155e98903a85df4
shalyne23 <shalynelangat@gmail.com>
2024-09-09 13:49:48 +0300

    person

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking")    
+ian=Person()
+ian.talk()
+tom=Person()
+tom.walk()
